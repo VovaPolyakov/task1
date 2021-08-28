0c89b8259b1e0374d08e3d24867c3ecd8a4ca94f
VovaPolyakov <1337vovalenovo@gmail.com>
2021-08-28 03:11:28 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const http = require('http');
 
 const server = http.createServer();
+
+server.on('request', (req, res) => {
+    res.end('Hello world');
+});
+
+server.listen(3000, '127.0.0.1', () => console.log('Сервер работает '));
